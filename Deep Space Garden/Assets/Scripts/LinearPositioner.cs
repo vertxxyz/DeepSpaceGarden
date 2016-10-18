ab1b4afef9c5d138e7a6b0c29f2d1722f142bd14
Thomas Ingram <thomasingram@vertx.xyz>
2016-10-18 21:22:00 +1100

    Lots of stuff, projectors and things to play with.

---
+++ Deep Space Garden/Assets/Scripts/LinearPositioner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class LinearPositioner : MonoBehaviour {
+	Transform _child;
+
+	Transform child {
+		get {
+			if (_child == null) {
+				if (transform.childCount > 0)
+					_child = transform.GetChild (0);
+			}
+			return _child;
+		}
+	}
+
+	public Transform positionRelativeTo;
+	
+	// Update is called once per frame
+	void Update () {
+		if (child == null || positionRelativeTo == null)
+			return;
+		Vector3 pos = Vector3.Project (positionRelativeTo.position - transform.position, transform.forward);
+		child.localPosition = pos;
+	}
+}
