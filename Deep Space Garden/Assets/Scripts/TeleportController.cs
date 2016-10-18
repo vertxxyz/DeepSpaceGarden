a44602afc841bdbf387851c1cf4699b9e57f2387
Thomas Ingram <thomasingram@vertx.xyz>
2016-10-18 16:13:00 +1100

    Added shader forge and started writing teleporter

---
+++ Deep Space Garden/Assets/Scripts/TeleportController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(SteamVR_TrackedController))]
+public class TeleportController : MonoBehaviour {
+
+	SteamVR_TrackedController controller;
+
+	void Start () {
+		controller = GetComponent<SteamVR_TrackedController> ();
+		controller.PadClicked += TrackpadButton;
+	}
+
+	
+	void TrackpadButton (object senderObj, ClickedEventArgs arguments) {
+
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, transform.forward, out hit, Mathf.Infinity)) {
+			NavMeshHit navHit;
+			if (NavMesh.FindClosestEdge (hit.point, out navHit, NavMesh.AllAreas)) {
+
+			}
+
+		}
+	}
+}
